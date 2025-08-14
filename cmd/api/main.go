da1448d6e8c7bffd18fb390c00870aee2dff7005
Sina Mahmudmoradi <sina.mahmudmoradi@gmail.com>
2025-08-14 15:13:49 +0330

    add logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
-
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"github.com/Sina-Mahmoodmoradi/ecommerce-backend/internal/http/middleware"
 	"github.com/Sina-Mahmoodmoradi/ecommerce-backend/internal/config"
+	"github.com/Sina-Mahmoodmoradi/ecommerce-backend/internal/pkg/logger"
 )
 
 func main() {
@@ -13,5 +16,21 @@ func main() {
 		log.Fatalf("Failed to load config: %v", err)
 	}
 
-	fmt.Printf("Running %s on port %s in %s mode\n", cfg.AppName, cfg.AppPort, cfg.AppEnv)
+	logger := logger.New(cfg.AppEnv)
+	defer logger.Sync()
+
+	route := gin.New()
+	route.Use(middleware.RequestID())
+	route.Use(middleware.GinLogger(logger))
+	route.Use(gin.Recovery())
+
+	route.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status": "ok",
+		})
+	})
+	logger.Info(fmt.Sprintf("Starting server on port %s", cfg.AppPort))
+	if err := route.Run(":" + cfg.AppPort); err != nil {
+		logger.Fatal("Failed to start server", zap.Error(err))
+	}
 }
